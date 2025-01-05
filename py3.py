28486e314ff4f174acc7fadc4692ca042c2b5ae0
Thassiane08 <thassiane321@hotmail.com>
2025-01-05 01:30:47 -0300

    update

---
+++ py3.py
@@ -0,0 +1,9 @@
+texte1 = input('dia?')
+text2 = input('mês?')
+text3 = input('ano?')
+
+
+
+textos = ('você nasceu no dia', texte1, text2, text3)
+
+print(textos)
---
+++ py4.py
@@ -0,0 +1,4 @@
+soma1 = input('digite um numero:')
+soma2 = input('digite outro numero:')
+
+print(soma1 + soma2)
