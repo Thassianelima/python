8c24fbd0096b08ab31b2de215e56e9d6dfff1f16
Thassiane08 <thassiane321@hotmail.com>
2025-01-08 19:46:42 -0300

    Create ex017.py

---
+++ ex017.py
@@ -0,0 +1,9 @@
+import math
+
+an = float(input('digite o angulo que você deseja:'))
+seno = math.sin(math.radians(an))
+print('o angulo de {} tem o seno de {:.2f}'.format(an, seno))
+cosseno = math.cos(math.radians(an))
+print('o angulo de {} tem o cosseno de {:.2f}'.format(an, cosseno))
+tangente = math.tan(math.radians(an))
+print('o angulo de {} tem a tangente de {:.2f}'.format(an, tangente))
