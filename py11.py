ec3491afb0bed87e0da6176cc0f437923f2ca2ac
Thassiane08 <thassiane321@hotmail.com>
2025-01-09 08:46:47 -0300

    Create py11.py

---
+++ py11.py
@@ -0,0 +1,7 @@
+frase = 'curso em video python'
+#print(frase[0:6])
+#print(frase.count('o'))
+#print(len(frase))
+#print(frase.replace('python','Android'))
+#print(frase.find('curso'))
+#print(frase.split())
