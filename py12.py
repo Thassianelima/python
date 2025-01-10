5db2f108ff0445952b8b3e63cc2880a8dac20b63
Thassiane08 <thassiane321@hotmail.com>
2025-01-09 23:09:06 -0300

    Create py12.py

---
+++ py12.py
@@ -0,0 +1,34 @@
+'''nome = str(input('qual é o seu nome?'))
+if nome == 'thassiane':
+    print('que nome lindo você tem!')
+else:
+    print('seu nome é tão normal!')
+print('bom dia, {}!'.format(nome))'''
+
+n1 = float(input('digite a primeira nota'))
+n2 = float(input('digite a segunda nota'))
+m = (n1 + n2)/2
+print('A sua media foi {}'.format(m))
+if m >= 6.0:
+    print(' A sua media foi boa! PARABENS!')
+else:
+    print('Sua media foi ruim!')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
